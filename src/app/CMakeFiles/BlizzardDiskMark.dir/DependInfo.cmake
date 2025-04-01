dc403577dc0e51d9ca4138ce87fe3d7b996dcf86
BlizzardWyvern <blizzardwyvern@web.de>
2025-04-02 00:02:07 +0200

    Enable debugging in VS Code

---
+++ src/app/CMakeFiles/BlizzardDiskMark.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "src/app/BlizzardDiskMark_autogen/timestamp" "custom" "src/app/BlizzardDiskMark_autogen/deps"
+  "/Users/alexander/CrystalDiskMark/src/app/AboutDlg.cpp" "src/app/CMakeFiles/BlizzardDiskMark.dir/AboutDlg.cpp.o" "gcc" "src/app/CMakeFiles/BlizzardDiskMark.dir/AboutDlg.cpp.o.d"
+  "/Users/alexander/CrystalDiskMark/src/app/BlizzardDiskMark_autogen/mocs_compilation.cpp" "src/app/CMakeFiles/BlizzardDiskMark.dir/BlizzardDiskMark_autogen/mocs_compilation.cpp.o" "gcc" "src/app/CMakeFiles/BlizzardDiskMark.dir/BlizzardDiskMark_autogen/mocs_compilation.cpp.o.d"
+  "/Users/alexander/CrystalDiskMark/src/app/DiskBench.cpp" "src/app/CMakeFiles/BlizzardDiskMark.dir/DiskBench.cpp.o" "gcc" "src/app/CMakeFiles/BlizzardDiskMark.dir/DiskBench.cpp.o.d"
+  "/Users/alexander/CrystalDiskMark/src/app/DiskMark.cpp" "src/app/CMakeFiles/BlizzardDiskMark.dir/DiskMark.cpp.o" "gcc" "src/app/CMakeFiles/BlizzardDiskMark.dir/DiskMark.cpp.o.d"
+  "/Users/alexander/CrystalDiskMark/src/app/DiskMarkDlg.cpp" "src/app/CMakeFiles/BlizzardDiskMark.dir/DiskMarkDlg.cpp.o" "gcc" "src/app/CMakeFiles/BlizzardDiskMark.dir/DiskMarkDlg.cpp.o.d"
+  "/Users/alexander/CrystalDiskMark/src/app/SettingsDlg.cpp" "src/app/CMakeFiles/BlizzardDiskMark.dir/SettingsDlg.cpp.o" "gcc" "src/app/CMakeFiles/BlizzardDiskMark.dir/SettingsDlg.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
