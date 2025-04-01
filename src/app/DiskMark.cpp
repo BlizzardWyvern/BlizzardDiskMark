092da54b6f2a61ec707524e1e41e0c20f1ac660c
BlizzardWyvern <blizzardwyvern@web.de>
2025-04-02 00:02:09 +0200

    Disable dark theme

--- src/app/DiskMark.cpp
+++ src/app/DiskMark.cpp
@@ -8,12 +8,26 @@ int main(int argc, char *argv[])
 {
 	QGuiApplication app(argc, argv);
 	QGuiApplication::setAttribute(Qt::AA_DontUseNativeMenuBar);
+	QPalette palette;
+	palette.setColor(QPalette::Window, Qt::white);
+	palette.setColor(QPalette::WindowText, Qt::black);
+	palette.setColor(QPalette::Base, Qt::white);
+	palette.setColor(QPalette::AlternateBase, Qt::lightGray);
+	palette.setColor(QPalette::ToolTipBase, Qt::white);
+	palette.setColor(QPalette::ToolTipText, Qt::black);
+	palette.setColor(QPalette::Text, Qt::black);
+	palette.setColor(QPalette::Button, Qt::lightGray);
+	palette.setColor(QPalette::ButtonText, Qt::black);
+	palette.setColor(QPalette::BrightText, Qt::red);
+	palette.setColor(QPalette::Link, Qt::blue);
+	palette.setColor(QPalette::Highlight, Qt::blue);
+	palette.setColor(QPalette::HighlightedText, Qt::white);
+	app.setPalette(palette);
 
 	qmlRegisterUncreatableType<ScoreLabel>("ScoreLabel", 1, 0, "ScoreLabel", "ScoreLabel is an attached property");
 
 	QQmlApplicationEngine engine;
 	engine.addImportPath(QStringLiteral("qrc:/"));
-	qDebug() << engine.importPathList();
 	engine.load(QUrl(QStringLiteral("qrc:/DiskMarkDlg.qml")));
 
 	if (engine.rootObjects().isEmpty())
